fcd99d4fa75bd1bc8f700a57c159d2198bf0fe13
Rafael Paz <rafaelpaz86@gmail.com>
2021-03-22 17:55:56 -0300

    Change main to call blocs.

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'package:bliss_challenge/blocs/emoji_bloc.dart';
+import 'package:bliss_challenge/blocs/repository_bloc.dart';
 import 'package:bliss_challenge/blocs/user_bloc.dart';
 import 'package:bliss_challenge/screens/splash_screen.dart';
 import 'package:bloc_pattern/bloc_pattern.dart';
@@ -9,6 +11,9 @@ void main() {
   runApp(BlocProvider(
     blocs: [
       Bloc((i) => UserBloc()),
+      Bloc((i) => EmojiBloc()),
+      Bloc((i) => RepositoryBloc()),
+
     ],
     child: GetMaterialApp(
       title: "Bliss Challenge",
