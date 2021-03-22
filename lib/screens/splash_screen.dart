dcc39e08dc014a57cdc06a238ba31865b12557f7
Rafael Paz <rafaelpaz86@gmail.com>
2021-03-22 17:04:24 -0300

    Add Splash Screen and Main Screen.

---
+++ lib/screens/splash_screen.dart
@@ -0,0 +1,41 @@
+
+import 'package:bliss_challenge/helpers/constants.dart';
+import 'package:bliss_challenge/helpers/strings.dart';
+import 'package:bliss_challenge/screens/main_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+class SplashScreen extends StatefulWidget {
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      child: Container(
+        color: textWhite,
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image.asset(
+                Strings.BLISS_LOGO,
+                scale: 1,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    Future.delayed(Duration(seconds: 8)).then((_) {
+      Get.offAll(MainScreen());
+    });
+  }
+}
