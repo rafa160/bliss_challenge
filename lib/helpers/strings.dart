e35adc8500e86c24db88c0a2bba063c0a889fad8
Rafael Paz <rafaelpaz86@gmail.com>
2021-03-22 17:05:03 -0300

    Add Helpers and constants.

---
+++ lib/helpers/strings.dart
@@ -0,0 +1,5 @@
+class Strings {
+
+  static const String BLISS_LOGO = 'assets/images/bliss.png';
+
+}
--- lib/main.dart
+++ lib/main.dart
@@ -9,10 +9,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
+      title: 'Bliss Challenge',
+      debugShowCheckedModeBanner: false,
       home: Container(),
     );
   }
