f238fb3fda47cf8650270832d9f3d23970f005da
Rafael Paz <rafaelpaz86@gmail.com>
2021-03-22 18:34:20 -0300

    Add more Strings, change main and create custom progess indicator.

--- lib/helpers/strings.dart
+++ lib/helpers/strings.dart
@@ -1,6 +1,40 @@
 class Strings {
 
-  static const String BLISS_LOGO = 'assets/images/bliss.png';
+  static const String APP_NAME = 'Bliss Challenge';
+  static const String HINT_USER_NAME_BUTTON = 'Search User';
+  static const String HINT_USER_TEXT = 'type username..';
   static const String BTN_BACK = 'back';
+  static const String HINT_SEARCH_RANDOM_EMOJI = 'Search Random Emoji';
+  static const String HINT_SEARCH_GOOGLE_REPOS = 'Google Repositories';
+  static const String HINT_FAV_EMOJIS = 'Favorite Emojis';
+  static const String HINT_FAV_AVATARS = 'Favorite Avatars';
+  static const String SEARCH_USER_REPOSITORIES = 'Search Repositories';
+  static const String HINT_SEARCH_EMOJI =  'Search Emojis';
+  static const String EMAIL_NOT_INFORMED = 'email not informed';
+  static const String NO_INFO = 'not informed';
+
+  static const String BTN_CLOSE =  'assets/images/btn_close.png';
+  static const String BLISS_LOGO = 'assets/images/bliss.png';
+
+  static const String SHARED_PREFERENCE_DELETED_AVATARS = 'deleted_avatars';
+  static const String SHARED_PREFERENCE_FAVORITE_AVATARS = 'fav_avatars';
+  static const String SHARED_PREFERENCE_DELETED = 'deleted';
+  static const String SHARED_PREFERENCE_FAVORITE = 'favorites';
+
+  static const String SUBTITLE_EMOJIS_BUTTON = 'Click to Search All Emojis';
+  static const String SUBTITLE_RANDOM_EMOJI = 'Click to Search Random Emoji';
+  static const String SUBTITLE_MY_FAV_EMOJIS = 'Check my Favorite Emojis';
+  static const String SUBTITLE_MY_FAV_AVATARS = 'Check my Favorite Avatars';
+
+  //Emojis flow Screens
+  static const String EMOJI_SCREEN_TITLE = 'Emojis';
+  static const String MY_FAVORITES_EMOJIS_TITLE = 'My Favorites Emojis';
+  static const String RANDOM_EMOJI_TITLE = 'Random Emoji';
+
+  // Avatars and Users Screens
+  static const String MY_FAVORITES_AVATARS_SCREEN_TITLE = 'My Favorites Avatars';
+  static const String PROFILE_TITLE_SCREEN = 'Profile';
+
+  static const String REPOSITORIES_TITLE_SCREEN = 'Repositories';
 
 }
--- lib/screens/main_screen.dart
+++ lib/screens/main_screen.dart
@@ -1,4 +1,18 @@
+
+import 'file:///home/rafa160/Downloads/bliss_challenge/lib/helpers/constants.dart';
+import 'package:bliss_challenge/blocs/emoji_bloc.dart';
+import 'package:bliss_challenge/blocs/repository_bloc.dart';
+import 'package:bliss_challenge/blocs/user_bloc.dart';
+import 'package:bliss_challenge/components/buttons/custom_buttom.dart';
+import 'package:bliss_challenge/components/main_card.dart';
+import 'package:bliss_challenge/components/random_card.dart';
+import 'package:bliss_challenge/helpers/strings.dart';
+import 'package:bloc_pattern/bloc_pattern.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_form_builder/flutter_form_builder.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:get/get.dart';
 
 class MainScreen extends StatefulWidget {
   @override
@@ -6,8 +20,130 @@ class MainScreen extends StatefulWidget {
 }
 
 class _MainScreenState extends State<MainScreen> {
+  var repositoryBloc = BlocProvider.getBloc<RepositoryBloc>();
+  var userBloc = BlocProvider.getBloc<UserBloc>();
+  var emojiBloc = BlocProvider.getBloc<EmojiBloc>();
+  var _controller = TextEditingController();
+
+  final GlobalKey<FormBuilderState> _formKey = GlobalKey<FormBuilderState>();
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    ScreenUtil.init(context);
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        title: Text(
+          Strings.APP_NAME,
+          style: appTitle,
+        ),
+        centerTitle: true,
+        elevation: 0,
+      ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: FormBuilder(
+            key: _formKey,
+            child: Column(
+              children: [
+                Card(
+                  elevation: 0,
+                  color: Colors.white,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(30.0),
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.only(
+                        left: 20.0, right: 20.0, bottom: 20.0),
+                    child: FormBuilderTextField(
+                      initialValue: null,
+                      name: 'name',
+                      validator: FormBuilderValidators.compose([
+                        FormBuilderValidators.required(context),
+                        FormBuilderValidators.minLength(context, 3)
+                      ]),
+                      textInputAction: TextInputAction.next,
+                      keyboardType: TextInputType.emailAddress,
+                      controller: _controller,
+                      decoration: InputDecoration(
+                          border: InputBorder.none,
+                          contentPadding:
+                          EdgeInsets.only(left: 20.0, top: 20.0),
+                          hintText: Strings.HINT_USER_TEXT,
+                          hintStyle: TextStyle(
+                              color: Color.fromRGBO(128, 128, 128, 0.6))),
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 4,
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Row(
+                    children: [
+                      CustomButton(
+                        onPressed: () async {},
+                        text: Strings.HINT_USER_NAME_BUTTON,
+                      ),
+                      Spacer(),
+                      CustomButton(
+                        onPressed: () async {
+                        },
+                        text: Strings.SEARCH_USER_REPOSITORIES,
+                      ),
+                    ],
+                  ),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    GestureDetector(
+                      onTap: () async {
+                      },
+                      child: MainCard(Strings.HINT_SEARCH_GOOGLE_REPOS, FontAwesomeIcons.google,
+                          Colors.red),
+                    ),
+                    GestureDetector(
+                        onTap: () async {
+                        },
+                        child: MainCard(
+                            Strings.HINT_SEARCH_EMOJI, FontAwesomeIcons.icons, Colors.greenAccent))
+                  ],
+                ),
+                GestureDetector(
+                    onTap: () async {},
+
+                    child: RandomCard(
+                        Strings.RANDOM_EMOJI_TITLE, FontAwesomeIcons.random, Colors.amberAccent)),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    GestureDetector(
+                        onTap: () async {
+
+                        },
+                        child: MainCard(
+                            Strings.HINT_FAV_EMOJIS, FontAwesomeIcons.heart, Colors.deepOrangeAccent)),
+                    GestureDetector(
+                        onTap: () async {
+
+                        },
+                        child: MainCard(
+                            Strings.HINT_FAV_AVATARS, FontAwesomeIcons.icons, Colors.indigoAccent)),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
