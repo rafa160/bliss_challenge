4c9c5a26e0c9d908267a800b748bba1813da3894
Rafael Paz <rafaelpaz86@gmail.com>
2021-03-22 17:15:53 -0300

    Add api endpoints.

---
+++ lib/services/github_api.dart
@@ -0,0 +1,8 @@
+class GitHubApi {
+
+  static const emojisRoute = 'https://api.github.com/emojis';
+  static const repoUserRoute = 'https://api.github.com/users/username';
+  static const userRepositoryRoute = 'https://api.github.com/users/';
+  static const googleRepositoryRoute = 'https://api.github.com/users/google/repos';
+  static const stringRepo = '/repos';
+}
