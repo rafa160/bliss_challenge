5576ddfdf3847da9317e184649290deca7a8a64b
Rafael Paz <rafaelpaz86@gmail.com>
2021-03-22 18:32:03 -0300

    Create more components cards and cached image.

---
+++ lib/components/main_card.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+class MainCard extends StatelessWidget {
+  final String title;
+  final IconData icon;
+  final Color color;
+
+  MainCard(this.title, this.icon, this.color);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      color: Colors.white,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(14.0),
+      ),
+      elevation: 4,
+      shadowColor: Colors.black26,
+      child: Container(
+        height: 140,
+        width: 140,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Icon(icon, size: 30, color: color),
+            SizedBox(
+              height: 10,
+            ),
+            Text(
+              title,
+              style: TextStyle(fontSize: 12),
+            ),
+            SizedBox(
+              height: 8,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
